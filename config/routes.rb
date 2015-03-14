8b0d990b50470be61b4a3884964f32cab5eefc21
Edgar Pabon <edgar.pabon@gmail.com>
2015-03-14 17:34:59 -0400

    Complete destroy assignment

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index] 
+  end
+
+  resources :posts, only: [] do
       resources :comments, only: [:create, :destroy]
     end
-  end
 
   get 'about' => 'welcome#about'
 
