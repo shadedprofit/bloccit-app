51cf952ca74048e2038cd0d0db5fd4f5542b723b
Edgar Pabon <edgar.pabon@gmail.com>
2015-03-23 16:31:06 -0600

    Completed Mocking with RSpec Checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ActiveRecord::Base
 
   default_scope { order('rank DESC') }
 
-  after_create :create_vote
 
   def up_votes
     votes.where(value: 1).count
@@ -30,10 +29,9 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
-  private
 
   def create_vote
     @vote = user.votes.create(value: 1, post: self)
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,6 +38,7 @@ topics = Topic.all
   )
 
    post.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+   post.create_vote
    post.update_rank
  end
  posts = Post.all
