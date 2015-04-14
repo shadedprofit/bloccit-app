c1bf82e7ac5b5805759902373ae5ac1d566f71cc
Edgar Pabon <edgar.pabon@gmail.com>
2015-04-14 15:48:52 -0500

    Added AJAX for use with destroying comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,5 @@
 class CommentsController < ApplicationController
-=begin
-  def new
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:post_id])
-    @comment = Comment.new
-    authorize @comment
-  end
 
-  def show
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-    authorize @comment
-  end
-
-  def edit
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-    authorize @comment
-  end
-=end
   def create
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
@@ -42,27 +21,16 @@ class CommentsController < ApplicationController
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was successfully deleted."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
-=begin
-  def update
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-    authorize @comment
-    if @comment.update_attributes(comment_params)
-      flash[:notice] = "Comment was updated."
-      redirect_to [@topic, @post]
-    else
-      flash[:error] = "There was an error saving the comment. Please try again."
-      render :edit
-    end
-  end
-=end
+
   private
 
   def comment_params
